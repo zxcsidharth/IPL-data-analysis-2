e67bd5983c8fe01c6795467b20aca22bbe878b0a
Sidharth Shankar <sidharth.shankar@mountblue.tech>
2020-03-12 22:31:48 +0530

    after modified 1st time

--- src/client/app.js
+++ src/client/app.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     const url = req.url;
     if(url === '/') {
-        fs.readFile("./index.html", function(err, data){
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-          });
+        fs.readFile("./index.html", function(err, data){    //this wil read the file and keep it in data 
+            res.writeHead(200, {'Content-Type': 'text/html'});  // writeHead returns/set the status code to the browser showing succes or failure
+            res.write(data);                                    //and also this will send a collection of headers to the browser
+            return res.end(); //write() will send the response body to the browser in form of whatever type specified i.e the html page to show in the client side.
+          });       //res.end() will show that the the response header and the body is sent to the client browser.
     }
     else if(url === '/matchesPlayedperYear.html') {
         fs.readFile("./matchesPlayedperYear.html", function(err, data){
